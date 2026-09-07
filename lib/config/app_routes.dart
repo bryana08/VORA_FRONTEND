84bcb02b0105f55b533110bfb7f4613bb72d03cd
bryana08 <bryana.nongni@facsciences-uy1.cm>
2026-09-07 15:47:15 +0100

    feat: connexion des routes et appel API sur inscription

---
+++ lib/config/app_routes.dart
@@ -0,0 +1,11 @@
+class AppRoutes {
+  static const String onboarding = '/';
+  static const String roleSelection = '/role-selection';
+  static const String registerPassenger = '/register-passenger';
+  static const String registerDriver = '/register-driver';
+  static const String passengerHome = '/passenger-home';
+  static const String rideNegotiation = '/ride-negotiation';
+  static const String passengerActiveRide = '/passenger-active-ride';
+  static const String driverHome = '/driver-home';
+  static const String driverValidation = '/driver-validation';
+}
